f3d9f9f7306ac4296cf3f08b4f860b623e729c14
Brian Monge <bm0801389031@gmail.com>
2021-07-11 18:51:18 -0700

    complete rock paper scissors

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-function computerPlay(){
-    let pick = ['rock', 'paper', 'scissors'];
-    return pick[Math.floor(Math.random() * pick.length)];
+
+function computerPlay() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const Random = Math.floor(Math.random() * choices.length);
+  return choices[Random];
 }
 
-//[Math.floor(Math.random() * pick.length)]
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return "Game Has Tied";
+  } else if (
+    (playerSelection.toLowerCase() === "rock" &&
+      computerSelection.toLowerCase() === "scissors") ||
+    (playerSelection.toLowerCase() === "paper" &&
+      computerSelection.toLowerCase() === "rock") ||
+    (playerSelection.toLowerCase() === "scissors" &&
+      computerSelection.toLowerCase() === "paper")
+  ) {
+    return "You Win";
+  } else {
+    return " computer wins";
+  }
+}
+
+function game() {
+  const playerSelection = prompt("Please Enter your selection");
+  const computerSelection = computerPlay();
+
+  console.log(playRound(playerSelection, computerSelection));
+}
